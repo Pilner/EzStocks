52d2a811955599e1e316a7fa0eb3c600fb7da73b
Jethro Pabroquez <92444260+Jetpabs@users.noreply.github.com>
2024-06-15 17:43:16 +0000

    Add Register page

---
+++ src/app/register.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './layout';
+import RegisterForm from '../_components/RegisterForm';
+
+const Register: React.FC = () => {
+  return (
+    <Layout>
+      <RegisterForm />
+    </Layout>
+  );
+};
+
+export default Register;
